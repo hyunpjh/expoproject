b109b93f0ea6b08a7ac13cb0de14e6357578673a
pjhyun <qkrwlgus4001@naver.com>
2023-01-29 15:17:47 +0900

    first upload

--- App.js
+++ App.js
@@ -1,20 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View , SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
 
 export default function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const login = (email, password) => {
+    if (email == 'park'){
+      if (password == 'hyun'){
+        alert(`success`);
+      }
+      else{
+        //alert(`email: ${email} password: ${password}`);
+        alert(`비밀번호를 체크해주세요.`);
+      }
+    }
+    else {
+      alert('이메일을 체크해주세요.');
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {StyleSheet.container}>
+      <Text style = {styles.baseText}>
+        LOGIN
+      </Text>
+
+      <TextInput style = {styles.textInput}
+        placeholder = "Email"
+        placeholderTextColor = "#ACB0B4"
+        onChangeText={text => setEmail(text)} value={email}
+      />
+
+      <TextInput style = {styles.textInput}
+        placeholder = "Password"
+        placeholderTextColor = "#ACB0B4"
+        onChangeText={text => setPassword(text)} value={password}
+      />
+    
+      <TouchableOpacity
+        style = {styles.submitButton}
+        onPress = {
+          () => login(email, password)
+        }>
+
+          <Text style = {styles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  baseText: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    color: '#405061',
+    marginTop: 60,
+    marginLeft: 20
   },
+
+  textInput:{
+    margin: 15,
+    height: 40,
+    borderColor: '#405061',
+    borderWidth: 1
+  },
+  submitButton:{
+    backgroundColor: '#405061',
+    padding: 10,
+    margin: 15,
+    height: 40
+  },
+  submitButtonText: {
+    color: '#D7DCE0'
+  },
+
 });
